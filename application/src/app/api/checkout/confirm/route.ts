a3110131a1998b966668f389591cec8ae0548079
Sayuru Akash Amarasinghe <sayuru555@gmail.com>
2023-08-20 16:33:21 +0530

    route update

--- application/src/app/api/checkout/confirm/route.ts
+++ application/src/app/api/checkout/confirm/route.ts
@@ -23,6 +23,21 @@ try {
   Reservation = mongoose.model("reservations", reservationSchema);
 }
 
+const checkoutSchema = new mongoose.Schema({
+  destination_id: String,
+  mode_id: String,
+  user_id: String,
+  departure_date: String,
+  passengers: Number,
+});
+
+let Checkout: any;
+try {
+  Checkout = mongoose.model("checkouts");
+} catch {
+  Checkout = mongoose.model("checkouts", checkoutSchema);
+}
+
 const uri: string =
   process.env.MONGODB_URI || "mongodb://localhost:27017/intergalactic-db";
 let dbConnection: any;
@@ -44,6 +59,8 @@ export async function POST(req: NextRequest, res: NextResponse) {
     const newReservation = await new Reservation(data);
     newReservation.status = "placed";
     const savedReservation = await newReservation.save();
+
+    await Checkout.deleteMany({ user_id: newReservation.user_id });
     return NextResponse.json({
       message: "Booking confirmed",
       id: savedReservation._id,
